ed58b532a10665b043fb60ffbc251152acd2bbd1
Kaarlo Kekkonen <kaarlo.kekkonen@codento.com>
2018-10-23 13:41:05 +0300

    added model for Request

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils import timezone
+
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+class Request(models.Model):
+    title = models.CharField(max_length=64)
+    consumer = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    maxPrice = models.FloatField(default=0)
+    minPrice = models.FloatField(default=0)
+    zipcode = models.IntegerField
+    description = models.TextField
+    timestamp = models.DateTimeField(editable=False, default=timezone.now)
--- app/views.py
+++ app/views.py
@@ -3,6 +3,10 @@ from app.forms import *
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
+from django.core import serializers
+from app.models import Request
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 # Create your views here.
 
@@ -40,6 +44,40 @@ def login(request):
         else:
             context = {'result_message': 'Invalid login! Please try again.'}
             return render(request, 'login.html', context)
+
+@login_required
+def create_request(request):
+    if(request.method == "POST"):
+        try:
+            category = Category.objects.get(pk=request.POST.get('category_id'))
+        except:
+            return HttpResponse("invalid category", status=400)
+        requestDict = {
+            'title': request.POST.get('title'),
+            'consumer': request.user,
+            'category': category,
+            'minPrice': request.POST.get('min_price'),
+            'maxPrice': request.POST.get('max_price'),
+            'description': request.POST.get('description'),
+            'zipcode': request.POST.get('zipcode'),
+            'timestamp': datetime.now(),
+        }
+        try:
+            request = Request(**dict)
+            request.save()
+            return HttpResponse(status=200)
+        except:
+            return HttpResponse(status=400)
+    else:
+        return HTTPResponse(statud=405)
+
+@login_required
+def my_requests(request):
+    if(request.method == "GET"):
+        myRequests = serializers.serialize('json', Reqeust.objects.filter(consumer=request.user).all())
+        return HTTPResponse(myRequests,status=200,content_type ="application/json")
+    else:
+        return HTTPResponse(statud=405)
 
 def logout(request):
     auth_logout(request)
