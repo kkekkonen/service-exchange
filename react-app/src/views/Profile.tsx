329c862e654f7fd7a20ff18bcd63ae2a31a089ca
peter <peter.sormunen@outlook.com>
2018-11-17 22:42:18 +0200

    Better profile view.

--- react-app/src/views/Profile.tsx
+++ react-app/src/views/Profile.tsx
@@ -6,8 +6,10 @@ import {ApiService} from '../services/apiservice'
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { createStyles } from '@material-ui/core';
@@ -26,11 +28,15 @@ interface IUserProfileProps extends WithStyles<typeof styles> {
 
 interface IState {
   userProfile: IUserProfile;
+  hasError: boolean;
+  errorText: string;
 };
 
 class Profile extends React.Component<IUserProfileProps, IState> {
   public state = {
     userProfile: {} as IUserProfile,
+    hasError: false,
+    errorText: "",
   };
   private apiService: ApiService;
   public constructor(props: IUserProfileProps) {
@@ -44,34 +50,49 @@ class Profile extends React.Component<IUserProfileProps, IState> {
     )
   }
 
-  public _handleChangeEvent(val: string) {
-    return val;
-  }
-
   public render() {
     const { classes } = this.props;
     return (
       <div className="Main">
         <Grid className={classes.root} container>
+          <Typography variant="h5" gutterBottom>
+            Edit profile
+          </Typography>
           <FormControl fullWidth={true}>
-            <TextField
-              label="Username"
-              value={this.state.userProfile.username}
-              defaultValue=" " 
-              onChange={()=>{this._handleChangeEvent(this.state.userProfile.username)}}
-            />
-            <TextField label="First name" value={this.state.userProfile.firstName} defaultValue=" " />
-            <TextField label="Last name" value={this.state.userProfile.lastName} defaultValue=" " />
-            <TextField label="Email" value={this.state.userProfile.email} defaultValue=" " />
-            <TextField label="Password" type="password" />
-            <TextField label="Password again" type="password" />
+            <TextField name="username" label="Username" onChange={this.handleProfileChange} value={this.state.userProfile.username || ''} required/>
+            <TextField name="firstName" label="First name" onChange={this.handleProfileChange} value={this.state.userProfile.firstName || ''} required />
+            <TextField name="lastName" label="Last name" onChange={this.handleProfileChange} value={this.state.userProfile.lastName || ''} required />
+            <TextField name="email" label="Email" onChange={this.handleProfileChange} value={this.state.userProfile.email  || ''} required />
             <Button variant="contained" color="primary" className="button">Save</Button>
+            {this.state.hasError && <FormHelperText>{this.state.errorText}</FormHelperText>}
           </FormControl>
         </Grid>
         <Divider />
+        <Grid className={classes.root} container>
+          <Typography variant="h5" gutterBottom>
+            Change password
+          </Typography>
+          <FormControl fullWidth={true}>
+            <TextField name="currentPassword" label="Current password" type="password" onChange={this.handleProfileChange} required />
+            <TextField name="newPassword"label="New password" type="password" onChange={this.handleProfileChange} required />
+            <TextField name="newPasswordAgain" label="New password again" type="password" onChange={this.handleProfileChange} required />
+            <Button variant="contained" color="primary" className="button">Change</Button>
+            {this.state.hasError && <FormHelperText>{this.state.errorText}</FormHelperText>}
+          </FormControl>
+        </Grid>
       </div>
     );
   }
+
+  private handleProfileChange = (event: {}) => {
+    const e = event as React.ChangeEvent<HTMLInputElement>;
+    const object = this.state.userProfile;
+    object[e.target.name] = e.target.value;
+    console.log(object)
+    this.setState({userProfile: object} as any);
+  }
+
+
 }
 
 export default withStyles(styles, { withTheme: true })(Profile);
