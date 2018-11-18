9658f3afb6277eb3dae97f43e3d626ace59c38ce
Vili Ketonen <vili.ketonen@aalto.fi>
2018-11-18 13:44:18 +0200

    Improved public profile page layout.

--- react-app/src/views/PublicProfile.tsx
+++ react-app/src/views/PublicProfile.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
 
 import {ApiService} from '../services/apiservice'
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import {IUserProfile} from '../models/models';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
@@ -17,10 +18,16 @@ const styles = (theme: Theme) =>
       marginBottom: "10px"
     },
     fieldUpper: {
-      fontWeight: "bold"
+      fontWeight: "normal",
+      color: "#0D9C6F"
     },
     fieldLower: {
 
+    },
+    detailsBox: {
+      padding: "1em",
+      backgroundColor: "#eee",
+      borderRadius: "4px"
     }
 });
 
@@ -55,18 +62,35 @@ class PublicProfile extends React.Component<IUserPublicProfileProps, IState> {
     return (
       <div>
       <Grid className={classes.root} container spacing={16}>
-        <Grid item xs={12}>
+        <Grid item md={6} xs={12}>
             <Typography variant="h4" gutterBottom>
               {this.state.userProfile.username}
             </Typography>
             <div className={classes.field}>
               <div className={classes.fieldUpper}>
-                Name
+                Full name
               </div>
               <div className={classes.fieldLower}>
                 {this.state.userProfile.firstName} {this.state.userProfile.lastName}
               </div>
             </div>
+            <div className={classes.field}>
+              <div className={classes.fieldUpper}>
+                Email address
+              </div>
+              <div className={classes.fieldLower}>
+                {this.state.userProfile.email}
+              </div>
+            </div>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Grid item xs={12} container spacing={16} className={classes.detailsBox}>
+            <Grid item xs={12}>
+              <Button href={`mailto:${this.state.userProfile.email}`} variant="contained" color="primary" className="button">
+                Send message
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
